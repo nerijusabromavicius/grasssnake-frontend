982630ff48a5d3bb44daacf8c35bce514b9bf628
Nerijus Abromavicius <nerijus.abromavicius@adform.com>
2018-05-22 16:17:40 +0300

    Foldure structure

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div> 
+    );
+  }
+}
